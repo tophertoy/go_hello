2ad619ce49976f21e074a9947f846515c518ec56
Chris Toynbee <ctoynbee@gmail.com>
2021-08-21 12:39:33 +1200

    greetings..

--- hello.go
+++ hello.go
@@ -3,11 +3,9 @@ package main
 import (
 	"fmt"
 
-	"example.com/greetings"
+	"rsc.io/quote"
 )
 
 func main() {
-	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
-	fmt.Println(message)
+	fmt.Println(quote.Go())
 }
